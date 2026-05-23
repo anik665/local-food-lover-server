09bb65a5fce8f2c115dae65b4a0e47e424f1802e
Anik Ghosh <anikgh7@gmail.com>
2026-05-23 21:02:49 +0600

    create my-reviews

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ async function run() {
 
     const database = client.db("localdb");
     const products = database.collection("products");
+    const favorites = database.collection("favorites");
+
+    //products api
 
     app.post("/products", async (req, res) => {
       const product = req.body;
@@ -37,6 +40,14 @@ async function run() {
       const result = await products.find().toArray();
       res.send(result);
     });
+
+    app.get("/latest-reviews", async (req, res) => {
+      const reviews = products.find().sort({ rating: -1 }).limit(6);
+      const result = await reviews.toArray();
+      res.send(result);
+    });
+
+    // Search api
     app.get("/reviews", async (req, res) => {
       const search = req.query.search || "";
       let query = {};
@@ -44,6 +55,63 @@ async function run() {
         query = { foodName: { $regex: search, $options: "i" } };
       }
       const result = await products.find(query).toArray();
+      res.send(result);
+    });
+
+    //favorites api
+    app.post("/favorites", async (req, res) => {
+      const favorite = req.body;
+      const result = await favorites.insertOne(favorite);
+      res.send(result);
+    });
+
+    app.get("/favorites", async (req, res) => {
+      const email = req.query.email;
+
+      const favoriteItems = await favorites.find({ email: email }).toArray();
+      const ids = favoriteItems.map((item) => new ObjectId(item.productId));
+
+      const result = await products.find({ _id: { $in: ids } }).toArray();
+
+      // প্রতিটা product এর সাথে favorites._id যোগ করুন
+      const finalResult = result.map((product) => {
+        const favItem = favoriteItems.find(
+          (fav) => fav.productId === product._id.toString(),
+        );
+        return {
+          ...product,
+          favoriteId: favItem._id.toString(), // এইটা নতুন যোগ হলো
+        };
+      });
+
+      res.send(finalResult);
+    });
+
+    app.delete("/favorites/:id", async (req, res) => {
+      try {
+        const productId = req.params.id;
+        const email = req.query.email; // email ও পাঠাতে হবে
+
+        const query = { _id: new ObjectId(productId), email: email };
+        console.log(query);
+        const result = await favorites.deleteOne(query);
+
+        console.log("DELETE RESULT:", result);
+        res.send(result);
+      } catch (error) {
+        res
+          .status(500)
+          .send({ message: "failed to delete favorite", error: error.message });
+      }
+    });
+
+    //my-reviews api
+    app.get("/my-reviews", async (req, res) => {
+      const email = req.query.email;
+      console.log(email, "fontent email");
+      const query = { userEmail: email };
+      const result = await products.find(query).toArray();
+      console.log(result, "result is this");
       res.send(result);
     });
 
